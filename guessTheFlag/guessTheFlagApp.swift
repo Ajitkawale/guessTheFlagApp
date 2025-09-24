e25b2df1e467fb96ee8dcb2f15711d01192966ac
Ajitkawale <Ajitkawale27@gmail.com>
2025-09-24 16:24:01 +0530

    Initial Commit

---
+++ guessTheFlag/guessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessTheFlagApp.swift
+//  guessTheFlag
+//
+//  Created by Ajit Kawale on 21/09/25.
+//
+
+import SwiftUI
+
+@main
+struct guessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
